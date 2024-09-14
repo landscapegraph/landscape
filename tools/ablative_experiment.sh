fc0586c7aa39d3079e5cc6b8573679ad2624bb55
Anon <anon@dontemailthis.org>
2024-09-14 18:00:34 -0400

    adjust repeats

--- tools/ablative_experiment.sh
+++ tools/ablative_experiment.sh
@@ -24,15 +24,15 @@ bash scale_experiment.sh $result_file 1 1 1 1 ', Cube + Standalone'
 
 python3 aws/run_first_n_workers.py --num_workers 16
 yes | bash setup_tagged_workers.sh $region 36 8
-bash scale_experiment.sh $result_file 16 16 1 3 ', Cube + Standalone'
+bash scale_experiment.sh $result_file 16 16 1 1 ', Cube + Standalone'
 
 python3 aws/run_first_n_workers.py --num_workers 32
 yes | bash setup_tagged_workers.sh $region 36 8
-bash scale_experiment.sh $result_file 32 32 1 5 ', Cube + Standalone'
+bash scale_experiment.sh $result_file 32 32 1 3 ', Cube + Standalone'
 
 python3 aws/run_first_n_workers.py --num_workers 48
 yes | bash setup_tagged_workers.sh $region 36 8
-bash scale_experiment.sh $result_file 48 48 1 7 ', Cube + Standalone'
+bash scale_experiment.sh $result_file 48 48 1 5 ', Cube + Standalone'
 
 
 # CAMEOSKETCH + STANDALONE
@@ -57,7 +57,7 @@ bash scale_experiment.sh $result_file 32 32 1 5 ', Cameo + Standalone'
 
 python3 aws/run_first_n_workers.py --num_workers 48
 yes | bash setup_tagged_workers.sh $region 36 8
-bash scale_experiment.sh $result_file 48 48 1 7 ', Cameo + Standalone'
+bash scale_experiment.sh $result_file 48 48 1 5 ', Cameo + Standalone'
 
 # Restore default settings
 cd ../build
